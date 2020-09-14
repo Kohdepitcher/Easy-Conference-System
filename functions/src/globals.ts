77d62e2ef5174537a309a05ec14b4fa1bb9cd4c5
Kohde Pitcher <kohde1@hotmail.com>
2020-09-15 00:59:39 +1000

    Created helper functions and enums

--- functions/src/globals.ts
+++ functions/src/globals.ts
@@ -1,10 +1,20 @@
+//
+
 
 //global enum to define types of auth roles
-enum AuthRoles {
+//models the available roles within the system
+export enum AuthRoles {
     Admin = "admin",
     presenter = "user"
 }
 
-// declare global {
-//     const Enums: typeof enums;
+// export enum userRole {
+//     user = "user",
+//     manager = "manager",
+//     admin = "admin"
 // }
+
+export function dateFromUTCString(s) {
+    s = s.split(/[-T:Z]/ig);
+    return new Date(Date.UTC(s[0], --s[1], s[2], s[3], s[4], s[5]));
+}
