6f5b0da63a507d62cb44387951f72c788f0773aa
Kohde Pitcher <kohde1@hotmail.com>
2020-09-15 01:06:41 +1000

    Update user controller to use the auth roles enum

--- functions/src/controllers/userController.ts
+++ functions/src/controllers/userController.ts
@@ -11,11 +11,7 @@ import { connect } from "../config";
 //import the user entity
 import { User } from "../entities/user";
 
-export enum userRole {
-    user = "user",
-    manager = "manager",
-    admin = "admin"
-}
+import { AuthRoles } from "../globals";
 
 export class userController {
 
@@ -23,6 +19,15 @@ export class userController {
     
     
     //CREATE
+    
+    /*
+        This function is responsible for creating users in the database directly without creating a firebase account
+        NOTE: this should only be used during development to create a user after creating a test user in firebase auth panel
+        It asks for the UUID of the new user to then create a matching entry in the DB
+
+        NOTE: this will not be needed after the on create firebase auth create user trigger is implemented
+        as it will automatically create a user itself and assign a role
+    */
     async createUserInDB(req: Request, res: Response) {
 
         try {
@@ -46,15 +51,15 @@ export class userController {
             //name is the display name
             newUser.name = name;
 
+            //set user email
             newUser.email = email;
 
+            //set user country
             newUser.country = country;
 
+            //set user timezone
             newUser.timeZone = timeZone;
 
-            
-            
-            
             //save the user in DB
             const savedUser = await repo.save(newUser);
             
@@ -71,22 +76,47 @@ export class userController {
 
     //creates a new user account and in database
     //requres a name, password, email, country, and timezone
+
+    /*
+        This function is responsible for creating a user in the system
+            It will create a fireauth user account using the provided details in the request body and assign a default role of user
+            It will also create a matching user entry in the DB using the provided details in the request body
+
+        NOTE: This query requires the following in the body:
+            name - becomes the user name and display name in fireauth
+            email - assigned to the DB record and firebase auth
+            country - assigned to the DB record
+            timeZone - assigned to tge db record
+
+        NOTE: Users created using this will have an automatic randomly generated password assigned to them
+        and if a user wants to use their account, they will have to reset their password
+
+        NOTE: This should only be used by an admin to create an account as it doesnt actually return the required auth details to log in on the client side
+        Users should use the fireauth web createuser api for the front end
+    */
     async createUser(req: Request, res: Response) {
         
         try {
             
             //default role that will be assigned to user
             //if the user is intended to be an admin, they can have their role update later on
-            const role = userRole.user;
+            const role = AuthRoles.presenter;
             
             //create a constant to store the request body
             const { name, email, country, timeZone } = req.body
             
             //if any of the required fields are empty
-            if (!name || !email) {
+            if (!email) {
                 
                 //send 400 error to client
-                return res.status(400).send({ message: 'Missing fields' })
+                return res.status(400).send({ message: 'Missing email for user' })
+            }
+
+            // no name in body
+            if (!name) {
+                
+                //send 400 error to client
+                return res.status(400).send({ message: 'Missing name for user' })
             }
 
             //no country in body
@@ -136,7 +166,14 @@ export class userController {
             //name is the display name
             newUser.name = name;
 
-            //TODO: add the other required fields
+            //set user email
+            newUser.email = email;
+
+            //set user country
+            newUser.country = country;
+
+            //set user timezone
+            newUser.timeZone = timeZone;
             
             
             //save the user in DB
@@ -187,29 +224,130 @@ export class userController {
     
     //UPDATE
     //update user
-    //TODO: include the other required properties when updating the user 
+
+    /*
+        This function is responsible for updating a specific user in both fireauth and DB
+
+        NOTE: if the user token attached to the request has a role of 'user', then they can only update their own record
+              if the user token attached has a admin role, then they can update any user using their UIDs
+
+        NOTE: only admins can assign roles to users
+    */
+
     async patch(req: Request, res: Response) {
         try {
-            const { uid } = req.params
-            const { displayName, email, role } = req.body
+
+            //temp store a UID that will be set soon
+            var specifiedUID: string;
+
+            //if a user is a presenter
+            if (res.locals.role == AuthRoles.presenter) {
+
+                //fetch the uid from the token instead
+                specifiedUID = res.locals.uid;
+
+            } 
             
-            if (!uid || !displayName || !email || !role) {
-                return res.status(400).send({ message: 'Missing fields' })
+            //user is a admin
+            else if (res.locals.role == AuthRoles.Admin) {
+
+                //if the uid is not set on the parameter when called by an admin
+                if (!req.params.uid) {
+                    return res.status(400).send({ message: 'Missing user UID when updating details by admin' })
+                }
+
+                //set the temp uid to be the one retrieved from the header
+                specifiedUID = req.params.uid;
+            }
+
+            //create a constant to store the request body
+            const { displayName, email, country, timeZone } = req.body
+
+            //if any of the required fields are empty
+            if (!email) {
+                
+                //send 400 error to client
+                return res.status(400).send({ message: 'Missing email for user' })
+            }
+
+            // no name in body
+            if (!displayName) {
+                
+                //send 400 error to client
+                return res.status(400).send({ message: 'Missing displayName for user' })
+            }
+
+            //no country in body
+            if (!country) {
+
+                //send error message to client
+                return res.status(400).send({ message: 'Missing country for user'})
+            }
+
+            //no timezone in budy
+            if (!timeZone) {
+
+                //return error message to client
+                return res.send(400).send({ message: 'Missing timezone for user'})
+            }
+
+                        
+            
+            //update the user information in fire auth
+            await admin.auth().updateUser(specifiedUID, { displayName, email })
+
+
+            //check if user calling this is an admin, if so they can change the role of the user
+            /* 
+                this next part assigns the role to a user if the calling user is an admin
+            */
+            if (res.locals.role == AuthRoles.Admin) {
+
+                //valid elements from the body
+                const { role } = req.body
+
+                if (!role) {
+                    return res.status(400).send({ message: 'Missing role from update'})
+                }
+
+                //check if the role from body exists in the auth roles enum
+                //if not return an error
+                if (!Object.values(AuthRoles).includes(role)) {
+                    return res.status(400).send({ message: 'Role: ' + role + " is not a valid role, must be either: " + AuthRoles.Admin + " or " + AuthRoles.presenter})
+                }
+
+                //if the user calling this endpoint is a admin, then update the role of the specificed user from request params
+                //update the user roles
+                await admin.auth().setCustomUserClaims(specifiedUID, { role })
             }
             
-            await admin.auth().updateUser(uid, { displayName, email })
-            await admin.auth().setCustomUserClaims(uid, { role })
-            
-            const user = await admin.auth().getUser(uid)
-            
+            //get and store the specific user from db
+            const user = await admin.auth().getUser(specifiedUID)
+
+
+            //update the user in the DB
+            //create a new connection
             const connection = await connect();
             
+            //create reference to the user repo
             const userRepo = connection.getRepository(User);
             
-            const updateUser = await userRepo.findOne({UUID: uid});
-            updateUser.name = displayName
+            //fetch the user from DB that matches the UID
+            const updateUser = await userRepo.findOne({UUID: specifiedUID});
+                       
+            //name is the display name
+            updateUser.name = displayName;
+
+            //set user email
+            updateUser.email = email;
+
+            //set user country
+            updateUser.country = country;
+
+            //set user timezone
+            updateUser.timeZone = timeZone;
             
-            
+            //save the changes to the DB
             await userRepo.save(updateUser);
             
             return res.status(204).send({ user: mapUser(user) })
@@ -221,7 +359,17 @@ export class userController {
     //DELETE
     //remove user
     //TODO: also delete the user from the DB
+
+    /* 
+        This function is responsible for deleting users from both fireauth and the db
+
+        NOTE: if the user token attached to the request has a role of 'user', then they can only delete their own record and account
+              if the user token attached has a admin role, then they can delete any user using their UIDs
+    */
     async  remove(req: Request, res: Response) {
+
+        // const userUID = determineTargetUIDFromRole(res, req.params.uid)
+
         try {
             const { id } = req.params
             await admin.auth().deleteUser(id)
@@ -249,3 +397,37 @@ function handleError(res: Response, err: any) {
             creationTime: user.metadata.creationTime
         }
     }
+
+
+    /*
+        This function determines which uid should be used based on the user's role from the token
+        It works by getting the uid from the response which has the uid, role, and email attached to it and then comparing the roles
+
+            if the user is an Admin, it will return the uid from the query paramters
+            otherwise, if the user is a presenter, it will return the UID from the token itself
+
+        NOTE: this in intended in routes where users should only be able to edit their own but admins can edit any user's data
+
+    */
+    // function determineTargetUIDFromRole(response: Response, uidFromQueryParam: string ) {
+
+    //     //if a user is a presenter
+    //     if (response.locals.role == AuthRoles.presenter) {
+
+    //         //fetch the uid from the token instead
+    //         return response.locals.uid;
+
+    //     } 
+        
+    //     //user is a admin
+    //     else if (response.locals.role == AuthRoles.Admin) {
+
+    //         //if the uid is not set on the parameter when called by an admin
+    //         if (!uidFromQueryParam) {
+    //             return response.status(400).send({ message: 'Missing user UID when updating details by admin' })
+    //         }
+
+    //         //set the temp uid to be the one retrieved from the header
+    //         return uidFromQueryParam;
+    //     }
+    // }
